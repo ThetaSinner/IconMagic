725dc0f8fa6230463c7025cf3aae38048d2aadee
ThetaSinner <greatdjonfire@hotmail.co.uk>
2015-08-02 00:12:33 +0100

    Added file IO
    The history manager is nearly done, just need to expose the data to the
    registry manager.

--- Source/RegistryHistory.cpp
+++ Source/RegistryHistory.cpp
@@ -1,4 +1,137 @@
+/** \file
+ * \brief Created by ThetaSinner (Gregory Jensen).
+ *        Released as open source.
+ *
+ * // TODO file_desc
+ */
+
 #include "RegistryHistory.hpp"
+
+// TODO remove temp includes
+#include <iostream>
+#include "WindowsToolbox.hpp"
+
+/** \brief Default constructor.
+ */
+RegistryHistory::RegistryHistory()
+{
+  this -> historyFileName = "";
+  this -> extensionHistory = std::vector<ExtensionEntry> ();
+}
+
+/** \brief Constructor to allow history file to be specified on object creation.
+ *
+ * \param history_file_name std::string
+ *
+ */
+RegistryHistory::RegistryHistory(std::string history_file_name)
+{
+  this -> historyFileName = "";
+  this -> extensionHistory = std::vector<ExtensionEntry> ();
+  this -> setHistoryFileName(history_file_name);
+}
+
+/** \brief Set the path to the file to be used for storing registry modification history.
+ *
+ * \param history_file_name std::string
+ * \return void
+ *
+ */
+void RegistryHistory::setHistoryFileName(std::string history_file_name)
+{
+  std::ifstream touch(history_file_name, std::ifstream::app);
+  touch.close();
+
+  if(GetFileAttributes(history_file_name.c_str()) == INVALID_FILE_ATTRIBUTES)
+  {
+    this -> historyFileName = "";
+    return;
+  }
+
+  this -> historyFileName = history_file_name;
+}
+
+/** \brief
+ *
+ * \return bool
+ *
+ */
+bool RegistryHistory::readHistory()
+{
+  if(historyFileName == "")
+  {
+    return false;
+  }
+
+  std::ifstream reader(historyFileName, std::ifstream::in);
+
+  if(!reader.is_open())
+  {
+    return false;
+  }
+
+  while(!reader.eof())
+  {
+    std::string lineOne, lineTwo;
+    reader >> lineOne;
+    reader >> lineTwo;
+
+    if(lineOne == "" || lineTwo == "")
+    {
+      break;
+    }
+
+    this -> extensionHistory.push_back(ExtensionEntry(lineOne + "\n" + lineTwo));    // this needs some way to fail safely.
+  }
+
+  reader.close();
+  return true;
+}
+
+bool RegistryHistory::writeHistory()
+{
+  if(historyFileName == "")
+  {
+    return false;
+  }
+
+  std::ofstream writer(historyFileName, std::ofstream::out);
+
+  if(!writer.is_open())
+  {
+    return false;
+  }
+
+  for(auto i : this -> extensionHistory)
+  {
+    writer << i.getFormatted() << "\n";
+  }
+
+  writer.close();
+  return true;
+}
+
+void RegistryHistory::addNewEntry(std::string extension_name, std::string image_name, std::string image_index)
+{
+  ImageEntry imageEntry;
+  imageEntry.createEntryFromRawData(image_name, image_index);
+  ExtensionEntry extensionEntry;
+  extensionEntry.createEntryFromRawData(extension_name, imageEntry);
+  this -> extensionHistory.push_back(extensionEntry);
+}
+
+void RegistryHistory::addImageEntryToExistingExtension(std::string existing_extension_name, std::string image_name, std::string image_index)
+{
+  for(auto &i : this -> extensionHistory)
+  {
+    if(i.getExtensionName() == existing_extension_name)
+    {
+      ImageEntry imageEntry;
+      imageEntry.createEntryFromRawData(image_name, image_index);
+      i.pushImageEntry(imageEntry);
+    }
+  }
+}
 
 /**
  * class ExtensionEntry
@@ -6,77 +139,99 @@
 
 ExtensionEntry::ExtensionEntry()
 {
-
 }
 
 ExtensionEntry::ExtensionEntry(std::string entry)
 {
-    loadEntryFromFormatted(entry);
+  loadEntryFromFormatted(entry);
+}
+
+void ExtensionEntry::createEntryFromRawData(std::string extension_name, ImageEntry image_entry)
+{
+  this -> extensionName = extension_name;
+  this -> imageHistory = std::vector<ImageEntry> ();
+  this -> imageHistory.push_back(image_entry);
 }
 
 void ExtensionEntry::createEntryFromRawData(std::string extension_name, std::vector<ImageEntry> image_history)
 {
-    this -> extensionName = extension_name;
-    this -> imageHistory = image_history;
+  this -> extensionName = extension_name;
+  this -> imageHistory = image_history;
 }
 
 void ExtensionEntry::loadEntryFromFormatted(std::string entry)
 {
-    this -> extensionName = entry.substr(0, entry.find("\n"));
+  this -> extensionName = entry.substr(0, entry.find("\n"));
+  std::string imageEntryLine = entry.substr(entry.find("\n") + 1);
 
-    std::string imageEntryLine = entry.substr(entry.find("\n") + 1);
-    do {
-        if (imageEntryLine[0] == '[')
-        {
-            std::string imageEntryString = imageEntryLine.substr(0, imageEntryLine.find("]") + 1);
-            this -> imageHistory.push_back(ImageEntry(imageEntryString));
-            imageEntryLine = imageEntryLine.substr(imageEntryString.size());
-        }
-        else if (imageEntryLine[0] == ',')
-        {
-            imageEntryLine = imageEntryLine.substr(1);
-        }
-        else
-        {
-            // failure.
-            break;
-        }
-    } while (imageEntryLine.size());
+  while(imageEntryLine.size())
+  {
+    if(imageEntryLine[0] == '[')
+    {
+      std::string imageEntryString = imageEntryLine.substr(0, imageEntryLine.find("]") + 1);
+      this -> imageHistory.push_back(ImageEntry(imageEntryString));
+      imageEntryLine = imageEntryLine.substr(imageEntryString.size());
+    }
+    else if(imageEntryLine[0] == ',')
+    {
+      imageEntryLine = imageEntryLine.substr(1);
+    }
+    else
+    {
+      // failure.
+      break;
+    }
+  }
 }
 
 std::string ExtensionEntry::getFormatted()
 {
-    std::string temp = "";
-    temp += extensionName;
-    temp += "\n";
-    int numberOfImageEntries = getNumberOfImageEntries();
-    for (int i = 0; i < numberOfImageEntries; i++)
-    {
-        temp += imageHistory[i].getFormatted();
-        if (i + 1 != numberOfImageEntries) temp += ",";
-    }
+  std::string temp = "";
+  temp += extensionName;
+  temp += "\n";
+  int numberOfImageEntries = getNumberOfImageEntries();
 
-    return temp;
+  for(int i = 0; i < numberOfImageEntries; i++)
+  {
+    temp += imageHistory[i].getFormatted();
+
+    if(i + 1 != numberOfImageEntries)
+    {
+      temp += ",";
+    }
+  }
+
+  return temp;
 }
 
 void ExtensionEntry::pushImageEntry(ImageEntry image_entry)
 {
-    imageHistory.push_back(image_entry);
+  imageHistory.push_back(image_entry);
 }
 
 int ExtensionEntry::getNumberOfImageEntries()
 {
-    return imageHistory.size();
+  return imageHistory.size();
 }
 
 ImageEntry ExtensionEntry::getImageEntryAt(int index)
 {
-    if (0 <= index && index < getNumberOfImageEntries())
-    {
-        return imageHistory[index];
-    }
+  if(0 <= index && index < getNumberOfImageEntries())
+  {
+    return imageHistory[index];
+  }
 
-    return ImageEntry();
+  return ImageEntry();
+}
+
+std::string ExtensionEntry::getExtensionName()
+{
+  return this -> extensionName;
+}
+
+std::vector<ImageEntry> ExtensionEntry::getImageHistory()
+{
+  return this -> imageHistory;
 }
 
 /**
@@ -85,39 +240,36 @@ ImageEntry ExtensionEntry::getImageEntryAt(int index)
 
 ImageEntry::ImageEntry()
 {
-
 }
 
 ImageEntry::ImageEntry(std::string entry)
 {
-    loadEntryFromFormatted(entry);
+  loadEntryFromFormatted(entry);
 }
 
 void ImageEntry::createEntryFromRawData(std::string image_path, std::string image_index)
 {
-    this -> imagePath = image_path;
-    this -> imageIndex = image_index;
+  this -> imagePath = image_path;
+  this -> imageIndex = image_index;
 }
 
 void ImageEntry::loadEntryFromFormatted(std::string entry)
 {
-    imagePath = entry.substr(1).substr(0, entry.find(",") - 1);
-    imageIndex = entry.substr(entry.find(",") + 1, entry.find("]") - entry.find(",") - 1);
+  imagePath = entry.substr(1).substr(0, entry.find(",") - 1);
+  imageIndex = entry.substr(entry.find(",") + 1, entry.find("]") - entry.find(",") - 1);
 }
 
 std::string ImageEntry::getFormatted()
 {
-    return ("[" + imagePath + "," + imageIndex + "]");
+  return ("[" + imagePath + "," + imageIndex + "]");
 }
 
-#ifdef DEBUG_BUILD
 std::string ImageEntry::getImagePath()
 {
-    return imagePath;
+  return imagePath;
 }
 
 std::string ImageEntry::getImageIndex()
 {
-    return imageIndex;
+  return imageIndex;
 }
-#endif // DEBUG_BUILD
--- Source/RegistryHistory.hpp
+++ Source/RegistryHistory.hpp
@@ -1,8 +1,18 @@
+/** \file
+ * \brief Created by ThetaSinner (Gregory Jensen).
+ *        Released as open source.
+ *
+ * // TODO file_desc
+ */
+
 #ifndef REGISTRYHISTORY_HPP_INCLUDED
 #define REGISTRYHISTORY_HPP_INCLUDED
 
 #include <string>
 #include <vector>
+#include <windows.h>
+#include <fstream>
+
 
 class ExtensionEntry;
 class ImageEntry;
@@ -10,67 +20,64 @@ class ImageEntry;
 class RegistryHistory
 {
 private:
-    std::string historyFileName;
-    std::vector<ExtensionEntry> extensionHistory;
+  std::string historyFileName;
+  std::vector<ExtensionEntry> extensionHistory;
 
 public:
-    RegistryHistory();
-    RegistryHistory(std::string history_file_name);
+  RegistryHistory();
+  RegistryHistory(std::string history_file_name);
 
-    void setHistoryFileName(std::string history_file_name);
+  void setHistoryFileName(std::string history_file_name);
 
-    bool readHistory();
-    bool writeHistory();
+  bool readHistory();
+  bool writeHistory();
 
-    void addNewEntry(std::string extension_name, std::string image_name, std::string image_index);
-    void addImageEntryToExistingExtension(std::string image_name, std::string image_index);
+  void addNewEntry(std::string extension_name, std::string image_name, std::string image_index);
+  void addImageEntryToExistingExtension(std::string existing_extension_name, std::string image_name, std::string image_index);
 
-    // bool selectEntryByExtensionName(std::string extName);
+  // bool selectEntryByExtensionName(std::string extName);
 };
 
 class ExtensionEntry
 {
 private:
-    std::string extensionName;
-    std::vector<ImageEntry> imageHistory;
+  std::string extensionName;
+  std::vector<ImageEntry> imageHistory;
 
 public:
-    ExtensionEntry();
-    ExtensionEntry(std::string entry);
+  ExtensionEntry();
+  ExtensionEntry(std::string entry);
 
-    void createEntryFromRawData(std::string extension_name, std::vector<ImageEntry> image_history);
-    void loadEntryFromFormatted(std::string entry);
-    std::string getFormatted();
+  void createEntryFromRawData(std::string extension_name, ImageEntry image_entry);
+  void createEntryFromRawData(std::string extension_name, std::vector<ImageEntry> image_history);
+  void loadEntryFromFormatted(std::string entry);
+  std::string getFormatted();
 
-    void pushImageEntry(ImageEntry image_entry);
-    int getNumberOfImageEntries();
-    ImageEntry getImageEntryAt(int index);
+  void pushImageEntry(ImageEntry image_entry);
+  int getNumberOfImageEntries();
+  ImageEntry getImageEntryAt(int index);
 
-    #ifdef DEBUG_BUILD
-    std::string getExtensionName();
-    std::vector<ImageEntry> getImageHistory();
-    #endif // DEBUG_BUILD
+  std::string getExtensionName();
+  std::vector<ImageEntry> getImageHistory();
 };
 
 class ImageEntry
 {
 private:
-    std::string imagePath;
-    std::string imageIndex;
+  std::string imagePath;
+  std::string imageIndex;
 
 public:
-    ImageEntry();
-    ImageEntry(std::string entry);
+  ImageEntry();
+  ImageEntry(std::string entry);
 
-    void createEntryFromRawData(std::string image_path, std::string image_index);
-    void loadEntryFromFormatted(std::string entry);
+  void createEntryFromRawData(std::string image_path, std::string image_index);
+  void loadEntryFromFormatted(std::string entry);
 
-    std::string getFormatted();
+  std::string getFormatted();
 
-    #ifdef DEBUG_BUILD
-    std::string getImagePath();
-    std::string getImageIndex();
-    #endif // DEBUG_BUILD
+  std::string getImagePath();
+  std::string getImageIndex();
 };
 
 #endif // REGISTRYHISTORY_HPP_INCLUDED
